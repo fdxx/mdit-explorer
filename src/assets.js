bd9b129b0580099bbdee73a9524f5a38729b58b4
fdxx <a778229661@gmail.com>
2026-07-25 21:39:02 +0800

    1

--- src/assets.js
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs';
-
-const stylesheet = fs.readFileSync(new URL('./style.css', import.meta.url), 'utf8');
-const browserScript = fs.readFileSync(new URL('./browser.js', import.meta.url), 'utf8');
-
-export const styles = `<style data-mdit-explorer-style>\n${stylesheet}</style>`;
-export const script = `<script data-mdit-explorer-script>\n${browserScript}</script>`;
